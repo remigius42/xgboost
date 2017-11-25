bf4a53ecd8de2c6ef91fefd52e76d5f31078af9c
Andreas Schmidt <andreas.remigius@gmail.com>
2017-11-25 18:02:48 +0100

    Set compiler explicitly in travis build

--- tests/travis/build_dylib.sh
+++ tests/travis/build_dylib.sh
@@ -6,7 +6,11 @@ make -f dmlc-core/scripts/packages.mk lz4
 if [ ${TASK} == "build_dylib" ]; then
     export PATH="/usr/local/bin:$PATH"
     echo $PATH
+    export CC=gcc-7
+    export CXX=g++-7
+    echo "gcc info"
     gcc --version
+    echo "end gcc info"
     cp make/config.mk config.mk
     #echo 'USE_OPENMP=0' >> config.mk
     echo 'TMPVAR := $(XGB_PLUGINS)' >> config.mk
