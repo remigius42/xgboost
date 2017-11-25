6d159bfa95595500d61b66b7cc8431cb18e53ff1
Andreas Schmidt <andreas.remigius@gmail.com>
2017-11-25 17:07:59 +0100

    Fix syntax error in travis script

--- tests/travis/build_dylib.sh
+++ tests/travis/build_dylib.sh
@@ -3,7 +3,7 @@
 cp make/travis.mk config.mk
 make -f dmlc-core/scripts/packages.mk lz4
 
-if [ ${TASK} == "build_dylib" }; then
+if [ ${TASK} == "build_dylib" ]; then
     cp make/config.mk config.mk
     #echo 'USE_OPENMP=0' >> config.mk
     echo 'TMPVAR := $(XGB_PLUGINS)' >> config.mk
