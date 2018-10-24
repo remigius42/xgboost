e379b5432a53f3fbafeb497596f8cba0765b3165
Andreas Schmidt <andreas.schmidt@ergon.ch>
2018-10-24 18:54:27 +0200

    Customize dylib building
    - Set whilelisted branch to current one (osx_build_v0.80).
    - Add debugging output.

--- tests/travis/build_dylib.sh
+++ tests/travis/build_dylib.sh
@@ -6,8 +6,11 @@ if [ ${TASK} == "build_dylib" ]; then
     cp make/config.mk ./config.mk
     sed -ie 's/# export CC = gcc/export CC = gcc-7/g' config.mk
     sed -ie 's/# export CXX = g++/export CC = g++-7/g' config.mk
+    echo "pwd:"
     pwd
+    echo "./config.mk"
     cat ./config.mk
+    echo "-----"
     
     export PATH="/usr/local/bin:$PATH"
     echo $PATH
@@ -15,6 +18,7 @@ if [ ${TASK} == "build_dylib" ]; then
     export CXX=g++-7
     echo "gcc info"
     g++-7 --version
+    echo "build start"
     make -j4
 fi
 
